967d0daa3b82c0b377829617a230fca996d5cae3
Rich Wandell <richwandell@gmail.com>
2019-12-23 23:08:29 -0500

    added some more tests

--- test/proc_scripts/process_test.php
+++ test/proc_scripts/process_test.php
@@ -2,7 +2,7 @@
 
 use aventri\Multiprocessing\Task;
 
-include realpath(__DIR__ . "/../../vendor") . "/autoload.php";
+include realpath(__DIR__ . "/../../vendor/") . "/autoload.php";
 
 (new class extends Task {
     public function consume($data)
